9b56dff1dbb5bd202ac22d42f50b053462d501c8
YU-CHE CHEN <job047587213@hotmail.com>
2020-09-26 16:02:02 +0800

    feat: add the login/register page

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const records = require('./modules/records')
 const home = require('./modules/home')
 const filter = require('./modules/filter')
+const users = require('./modules/users')
 
-router.use('/records', records)
 router.use('/', home)
+router.use('/records', records)
 router.use('/filter', filter)
+router.use('/users', users)
 
 module.exports = router
